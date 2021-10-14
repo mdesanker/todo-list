d772a75a288e8d9de437084e915c33f5578ff9f3
Michael Desanker <mdesanker@gmail.com>
2021-10-14 19:58:23 -0400

    feat: add html and css for list display

--- src/index.js
+++ src/index.js
@@ -2,4 +2,20 @@
 
 import "./style.css";
 
-console.log("Test log");
+// Elements
+const projectItems = document.querySelectorAll(".project-item");
+
+const MODULE = (function () {
+  projectItems.forEach((proj) => {
+    proj.addEventListener("mouseover", function (e) {
+      const project = e.target.closest("button");
+      const exitIcon = project.querySelector(".fa-times-circle");
+      exitIcon.classList.remove("hidden");
+    });
+    proj.addEventListener("mouseout", function (e) {
+      const project = e.target.closest("button");
+      const exitIcon = project.querySelector(".fa-times-circle");
+      exitIcon.classList.add("hidden");
+    });
+  });
+})();

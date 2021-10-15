b6fb25be71dcc4c42235e2c56c063ce562dd42ad
Michael Desanker <mdesanker@gmail.com>
2021-10-15 06:30:20 -0400

    feat: add html and css for sidebar project creation form

--- src/index.js
+++ src/index.js
@@ -19,3 +19,37 @@ const MODULE = (function () {
     });
   });
 })();
+
+// Sidebar form to create new project array
+
+// Todo objects will be instances of a class
+
+class Todo {
+  constructor(title, dueDate, description, priority) {
+    this.title = title;
+    this.dueDate = dueDate;
+    this.description = description;
+    this.priority = priority;
+  }
+}
+
+const item1 = new Todo("Grocery shopping", "10/16/21");
+const item2 = new Todo("Wash dishes");
+console.log(item1);
+console.log(item2);
+
+// Projects will be an array of todo objects
+
+const project1 = [];
+project1.push(item1);
+project1.push(item2);
+
+console.log(project1);
+
+// Project array items will be output to the display
+
+project1.forEach((item) => {
+  console.log(`${item.title}`);
+});
+
+// Everytime todo item added to project, refresh display

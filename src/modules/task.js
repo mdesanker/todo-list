1691980eb30a0663119261834eae843ebc8f05b8
Michael Desanker <mdesanker@gmail.com>
2021-10-18 10:59:49 -0400

    feat: add module for creating tasks

---
+++ src/modules/task.js
@@ -0,0 +1,24 @@
+class Task {
+  constructor(name, date = "No date") {
+    this.name = name;
+    this.date = date;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setDate(date) {
+    this.date = date;
+  }
+
+  getDate() {
+    return this.date;
+  }
+}
+
+export { Task };

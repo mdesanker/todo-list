3c9fa3bba887a583f9b34e490191bf5ba6ac4cb6
Michael Desanker <mdesanker@gmail.com>
2021-10-20 16:13:37 -0400

    feat: load and store user projects in local storage

--- src/modules/display.js
+++ src/modules/display.js
@@ -31,23 +31,53 @@ const taskPriorityInput = document.querySelector("#task-priority");
 const taskSubmitBtn = document.querySelector(".task-submit");
 const taskCancelBtn = document.querySelector(".task-cancel");
 
-// Default to "Today" project
-let activeProjectIndex = 0;
+// Default To Do list
+const defaultToDo = new ProjectList();
 
-///////////////////////////////////////////////
-// Test projects
-const toDoList = new ProjectList();
-
-const project1 = new Project("Project 1");
+const project1 = new Project("Chores");
 const project2 = new Project("Groceries");
 const dishes = new Task("Wash dishes", "10/20/2021");
 project1.addTask(dishes);
 
-toDoList.addProject(project1);
-toDoList.addProject(project2);
-///////////////////////////////////////////////
+defaultToDo.addProject(project1);
+defaultToDo.addProject(project2);
+
+// Default to "Today" project
+let activeProjectIndex = 0;
+let myToDo = defaultToDo;
 
 // FUNCTIONS
+function storeLocal(todoList) {
+  localStorage.setItem("storedToDo", JSON.stringify(todoList));
+}
+
+function retrieveLocal() {
+  console.log("default", defaultToDo.getProjectAtIndex(2).getTasks());
+  const data = JSON.parse(localStorage.getItem("storedToDo"));
+
+  if (data) {
+    console.log("storedToDo", data);
+    console.log(Object.entries(data)[0][1]);
+    const newProjList = new ProjectList();
+    Object.entries(data)[0][1].forEach((item, index) => {
+      console.log(item.taskList);
+      const newProject = new Project(item.name);
+      console.log("item task", item.taskList);
+      item.taskList.forEach((task) => {
+        const newTask = new Task(task.name, task.date);
+        if (task.isComplete) newTask.changeCompleteStatus();
+        newProject.addTask(newTask);
+      });
+      // newProject.setTasks(item.taskList);
+      console.log("new project", newProject);
+      if (index > 1) newProjList.addProject(newProject);
+    });
+
+    console.log(newProjList);
+    myToDo = newProjList;
+  } else myToDo = defaultToDo; // If no stored data, use default
+}
+
 function openProjectForm() {
   addProjectBtn.classList.add("not-visible");
   projectFormContainer.classList.remove("not-visible");
@@ -76,9 +106,12 @@ function closeTaskForm() {
   clearTaskForm();
 }
 
+// Check local storage
+retrieveLocal();
+
 function initializeWebsite() {
   updateProjectList();
-  updateProjectDisplay(toDoList.getProjectAtIndex(activeProjectIndex));
+  updateProjectDisplay(myToDo.getProjectAtIndex(activeProjectIndex));
 }
 
 function updateProjectList() {
@@ -89,7 +122,7 @@ function updateProjectList() {
   const projectList = makeElement("div", ["project-list"]);
 
   // Generate buttons for user project list
-  const currentProjects = toDoList.getProjects();
+  const currentProjects = myToDo.getProjects();
   currentProjects.forEach((project, index) => {
     if (project.getName() !== "Today" && project.getName() !== "Week") {
       const projectBtn = makeElement(
@@ -112,11 +145,12 @@ function updateProjectList() {
     if (!clicked) return;
     if (clicked.classList.contains("project-item")) {
       activeProjectIndex = e.target.closest("button").dataset.id;
-      updateProjectDisplay(toDoList.getProjectAtIndex(activeProjectIndex));
+      updateProjectDisplay(myToDo.getProjectAtIndex(activeProjectIndex));
     }
   });
 
   userProjectContainer.prepend(projectList);
+  // storeLocal(myToDo);
 }
 
 function updateProjectDisplay(project) {
@@ -124,7 +158,7 @@ function updateProjectDisplay(project) {
   document.querySelector(".list-container").remove();
 
   // Update title
-  displayTitle.textContent = toDoList
+  displayTitle.textContent = myToDo
     .getProjectAtIndex(activeProjectIndex)
     .getName();
   if (activeProjectIndex < 2) {
@@ -183,23 +217,26 @@ function updateProjectDisplay(project) {
     const targetItem = e.target.closest("li").dataset.id;
     // Delete item
     if (e.target.classList.contains("fa-trash-alt")) {
-      toDoList
+      myToDo
         .getProjectAtIndex(activeProjectIndex)
         .removeTaskAtIndex(targetItem);
-      updateProjectDisplay(toDoList.getProjectAtIndex(activeProjectIndex));
+      updateProjectDisplay(myToDo.getProjectAtIndex(activeProjectIndex));
     }
     // Mark complete
     else if (
       e.target.classList.contains("fa-square") ||
       e.target.classList.contains("fa-check-square")
     ) {
-      toDoList
+      myToDo
         .getProjectAtIndex(activeProjectIndex)
         .getTaskAtIndex(targetItem)
         .changeCompleteStatus();
-      updateProjectDisplay(toDoList.getProjectAtIndex(activeProjectIndex));
+      updateProjectDisplay(myToDo.getProjectAtIndex(activeProjectIndex));
     }
+    storeLocal(myToDo);
   });
+
+  // storeLocal(myToDo);
 }
 
 // EVENT LISTENERS
@@ -209,20 +246,22 @@ projectCancelBtn.addEventListener("click", closeProjectForm);
 projectForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const newProject = projectInputField.value;
-  if (toDoList.checkProjectName(newProject))
-    toDoList.addProject(new Project(newProject));
+  if (myToDo.checkProjectName(newProject))
+    myToDo.addProject(new Project(newProject));
   else alert(`${newProject} already in use.`);
   closeProjectForm();
   updateProjectList();
+  storeLocal(myToDo);
 });
 
 // Delete project
 deleteProjBtn.addEventListener("click", function () {
-  console.log("Delete project", toDoList.getProjectAtIndex(activeProjectIndex));
-  toDoList.removeProject(toDoList.getProjectAtIndex(activeProjectIndex));
+  console.log("Delete project", myToDo.getProjectAtIndex(activeProjectIndex));
+  myToDo.removeProject(myToDo.getProjectAtIndex(activeProjectIndex));
   activeProjectIndex--;
-  updateProjectDisplay(toDoList.getProjectAtIndex(activeProjectIndex));
+  updateProjectDisplay(myToDo.getProjectAtIndex(activeProjectIndex));
   updateProjectList();
+  storeLocal(myToDo);
 });
 
 // Add task form
@@ -233,9 +272,10 @@ taskForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const newTask = new Task(taskTitleInput.value, taskDateInput.value);
 
-  toDoList.getProjectAtIndex(activeProjectIndex).addTask(newTask);
+  myToDo.getProjectAtIndex(activeProjectIndex).addTask(newTask);
   closeTaskForm();
-  updateProjectDisplay(toDoList.getProjectAtIndex(activeProjectIndex));
+  updateProjectDisplay(myToDo.getProjectAtIndex(activeProjectIndex));
+  storeLocal(myToDo);
 });
 
 export { initializeWebsite };
